ca8f465f6eac6fa8ad46b749c8686da9598d32bf
Shiva Pasham <spasham@planittesting.com>
2018-06-15 19:10:03 +0100

    failure case

--- src/test/java/tests/HomePageTest.java
+++ src/test/java/tests/HomePageTest.java
@@ -29,7 +29,7 @@ public class HomePageTest extends BrowserSetup {
 
     @Test(priority=1)
     public void tab_coffeeClub() throws Exception {
-        verifyTab("Coffee Club");
+        verifyTab("Coffee Clubxx");
     }
 
     @Test (priority=2)
